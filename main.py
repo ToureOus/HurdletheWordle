92db64e0829ba7424dadefd1fab6c3b642bad80c
ToureOus <102052479+ToureOus@users.noreply.github.com>
2022-04-01 15:50:43 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from Words import dictionary_possible_words
+
+## pip install selenium
+
+wDict = dictionary_possible_words
+
+
+
