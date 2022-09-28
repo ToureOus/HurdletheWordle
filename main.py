861ebc286d71ef1761578e0531c4b3fc1a4ab69c
Toureous <Ousmane.toure1998@gmail.com>
2022-09-28 12:21:36 -0400

    9/28 Correct_stats : after bot completes wordle, function opens csv file and searches for the correct word. If word already in file it will not write it to the file again.
    PS. Correct in 3

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import time
-import csv
 from datetime import datetime
 import json
+import csv
 from selenium import webdriver
-
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from Words import dictionary_possible_words
+
 
 # from selenium.webdriver.support import expected_conditions as ec
 # from selenium.webdriver.support.ui import WebDriverWait
@@ -15,6 +15,7 @@ from Words import dictionary_possible_words
 #
 class Wurdle:
     """Setup Class    """
+
     def __init__(self):
         self.driver = driver = webdriver.Chrome()
         self.driver.get("https://www.nytimes.com/games/wordle/index.html")
@@ -25,7 +26,7 @@ class Wurdle:
 
         # closeicon = driver.find_element(By.CLASS_NAME, "Modal-module_closeIcon__b4z74")
         # action.find_element(By.CLASS_NAME, "Modal-module_closeIcon__b4z74").click().perform()
-        driver.find_element(By.CLASS_NAME, "Modal-module_closeIcon__b4z74").click() #simplified popup close icon.
+        driver.find_element(By.CLASS_NAME, "Modal-module_closeIcon__b4z74").click()  # simplified popup close icon.
         time.sleep(1)
         self.page = driver.find_element(By.TAG_NAME, "html")
         self.page.click()
@@ -46,28 +47,30 @@ class Wurdle:
         evaluator = json.loads(game_info)["evaluations"]  # friend jason loads the game info and returns
         return evaluator
 
+    def correct_stats(self, word):
+        """After the Bot has completed the puzzle, it will upload its data to a csv file"""
+        data = [datetime.now().date(), word, self.count]
+        w = str(word)
+        with open('worddata.csv', 'r') as f:
+            s = f.read() #reads csv file completely
+        if w not in s: #if word not in CSV s ^ then reopen the file
+            with open('worddata.csv', 'a+') as f:
+                writer = csv.writer(f) #csv writer declaration
+                writer.writerow(data) #write data line to csv
+        else:
+            print("Word has already been solved and added to worddata.csv")
+
 
 class Bot(Wurdle):
     """main program, iterates through list of words using Wurdle setup parameters, guessing and evaluating as it goes"""
-    def stats(self, word):
-        """After the Bot has completed the puzzle, it will upload its data to a csv file"""
-        head = ['date', 'word', 'Attempts count']
-        data = [datetime.now(), word, self.count]
-        with open('worddata.csv', 'a') as f:
-            writer = csv.writer(f)
-            # writes the header
-            # writer.writerow(head) #uncomment to make a new csv file.
-            # writes the data
-            writer.writerow(data)
 
     def eval(self):
         """inputs selected two guesses and edits our possible words list based off the response of evaluator,
          inputs a most probable answer after"""
-        guesses =   ['soare', 'clint']
-
+        guesses = ['soare', 'clint']
 
         # ['brown', 'shade']
-        # 'two possible word combinations.
+        # two possible word combinations.
         possible_words = self.wDict
         known_l = []
         # known letters list
@@ -85,7 +88,6 @@ class Bot(Wurdle):
                 elif stats[i] == "correct":
                     correct_tally += 1
             if correct_tally == 5:
-
                 time.sleep(2.5)
                 self.page.click()
                 quit()
@@ -112,7 +114,6 @@ class Bot(Wurdle):
             while type(evaluator[q + 1]) is not list:
                 try:
                     for i in range(5):
-
                         self.page.send_keys(Keys.BACKSPACE)
                         possible_words.remove(word)
                         word = possible_words[0]
@@ -120,8 +121,8 @@ class Bot(Wurdle):
 
                         evaluator = self.get_data()
 
-                except Exception:
-                    self.stats(word)
+                finally:
+                    self.correct_stats(word)
                     return self.count
 
         time.sleep(2.5)
@@ -130,7 +131,6 @@ class Bot(Wurdle):
 
 wurdle = Bot()
 wurdle.eval()
-
 
 # 92% success rate since April 13th
 # edited by request to increase competitiveness, now guesses in 3 rather than 2.
